8dfb1f492cdc2ce4ea7e61ea99212cb363f1ab8f
김동환 <kimdonghwan@gimdonghwans-Mac-mini.local>
2025-08-27 19:31:35 +0900

    프로젝트 생성 및 초기화

---
+++ WeatherColor/Sources/WeatherColor/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WeatherColor
+//
+//  Created by 김동환 on 8/16/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .white
+  }
+}
