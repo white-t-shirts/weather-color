7b41b24cf9324e3e9b21015383938ba4e60b8155
Donghwan Kim <supersupremekim@gmail.com>
2025-09-19 14:36:11 +0900

    Network 라이브러리 추가

---
+++ Projects/AppFeature/AppFeature_Home/Sources/Implement/Data/API/PrivatKey/PrivateKey.swift
@@ -0,0 +1,8 @@
+//
+//  PrivateKey.swift
+//  AppFeature_Home_Impl
+//
+//  Created by 김동환 on 9/19/25.
+//
+
+import Foundation
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -10,12 +10,21 @@ let packageSettings = PackageSettings()
 let package = Package(
   name: "WeatherColor",
   dependencies: [
+    // Rx
     .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
     .package(url: "https://github.com/ReactorKit/ReactorKit.git", from: "3.0.0"),
-    .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0"),
+
+    // UI
     .package(url: "https://github.com/layoutBox/PinLayout.git", from: "1.0.0"),
     .package(url: "https://github.com/layoutBox/FlexLayout.git", from: "2.2.2"),
+
+    // Foundation
+    .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0"),
     .package(url: "https://github.com/Swinject/Swinject.git", from: "2.0.0"),
     .package(url: "https://github.com/devxoul/Pure.git", from: "1.1.4"),
+
+    // Network
+    .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
+    .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.4.3"),
   ]
 )
